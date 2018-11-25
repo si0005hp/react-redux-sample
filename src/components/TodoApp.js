006123c570a64376ff36effee9035a8f0d732e51
si0005hp <si0005hp@gmail.com>
2018-11-25 13:54:35 +0800

    react-redux

---
+++ src/components/TodoApp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function TodoApp({ task, tasks, inputTask, addTask }) {
+  return (
+    <div>
+      <input type="text" onChange={(e) => inputTask(e.target.value)} />
+      <input type="button" value="add" onClick={() => addTask(task)} />
+      <ul>
+        {tasks.map((item, i) => <li key={i}>{item}</li>)}
+      </ul>
+    </div>
+  );
+}
